db5ebdf14f56aa3e9c9e5a10b813c6153a0eb781
kingauther33 <kingauther33@gmail.com>
2022-02-16 11:19:11 +0700

    update assignment 3 ss2

--- ss1-setup/basics-assignment-3-start/src/app/app.component.ts
+++ ss1-setup/basics-assignment-3-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isDisplayingParagraph = true;
+  logs = [];
+  numOfLogs = 0;
+
+  toggleParagraph() {
+    this.isDisplayingParagraph = !this.isDisplayingParagraph;
+    this.numOfLogs++;
+    this.logs.push(new Date());
+  }
+
+  checkLogs() {
+    return this.numOfLogs >= 5;
+  }
 }
